98514d728d1a28f10af4249d5535aa7137c411d6
Tetiana Hv <tetiana.qaengineer@gmail.com>
2020-10-31 19:22:40 +0200

    ComparisonTest done

--- src/test/java/citrus/ComparisonTest.java
+++ src/test/java/citrus/ComparisonTest.java
@@ -3,20 +3,17 @@ package citrus;
 import citrusPages.ComparisonPage;
 import citrusPages.HomePage;
 import citrusPages.ProductListPage;
-import citrusPages.ProductPage;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.refresh;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 public class ComparisonTest {
     HomePage homePage;
     ProductListPage productListPage;
-    ProductPage productPage;
     ComparisonPage comparisonPage;
 
     String mainMenuName = "Ноутбуки, планшеты, МФУ";
@@ -34,15 +31,13 @@ public class ComparisonTest {
 
     @Test
     public void addTwoAndOneProductsToComparison() {
-        //1) Click Notebooks/Acer in menu
         homePage.waitForPageToLoad()
                 .closePopup()
                 .hoverMenuLine(mainMenuName)
                 .clickLinkInMenu(menuName);
         productListPage.waitForPageToLoad()
                 .closePopup();
-        //2) Add first and second laptop to comparison
-        // (no navigation to product page). Remember names, prices
+
         String productName1 = productListPage.getProductNameByCount(0, menuName);
         String productPrice1 = productListPage.getProductPriceByName(productName1);
         productListPage.addProductToComparison(productName1)
@@ -53,28 +48,25 @@ public class ComparisonTest {
         productListPage.addProductToComparison(productName2)
                 .waitForPageToLoad();
 
-        //3) Click on comparison icon in header
         productListPage.getHeaderFragment().clickOnMainComparisonIcon();
         comparisonPage.waitForPageToLoad()
                 .closePopup();
-//                - only 2 products in comparison
-//                - prices and names of products are correct
+
         assertEquals(comparisonPage.getComparisonItemsListSize(), 2);
         assertEquals(comparisonPage.getComparisonItemsNameByCount(0), productName1);
         assertEquals(comparisonPage.getComparisonItemsNameByCount(2), productName2);
         assertEquals(comparisonPage.getComparisonItemsPriceByCount(0), productPrice1);
         assertEquals(comparisonPage.getComparisonItemsPriceByCount(2), productPrice2);
-//        5) Click 'add new product to comparison'
-        //        6) Choose first (remember name, price)
+
         comparisonPage.getProductSelectionFragment().clickAddNewProductButton();
         String productName3 = comparisonPage.getProductSelectionFragment().getProductNameByCount(0);
         String productPrice3 = comparisonPage.getProductSelectionFragment().getProductPriceByCount(0);
-
         comparisonPage.getProductSelectionFragment().clickNewProductCheckboxByCount(0)
                 .clickAddButton();
         comparisonPage.waitForPageToLoad()
                 .closePopup();
 
+        Selenide.sleep(50);
         assertEquals(comparisonPage.getComparisonItemsListSize(), 3);
         assertEquals(comparisonPage.getComparisonItemsNameByCount(0), productName1);
         assertEquals(comparisonPage.getComparisonItemsNameByCount(4), productName2);
